c8ca0bdd33ad67a4dada0b7e53f0d9e8798e44b5
mattesalva93 <salvalaggiomatteo93@gmail.com>
2022-02-08 15:39:29 +0100

    jumbotron structure done

--- src/components/sections/Jumbotron.vue
+++ src/components/sections/Jumbotron.vue
@@ -1,6 +1,17 @@
 <template>
 <div class="ms_jumbotron">
-    
+    <div class="ms_jumbotron-text">
+        <h1>
+            Brand New Arrivals
+        </h1>
+        <h3 class="mt-3">
+            NEW COLLECTION FROM NEW YORK
+        </h3>
+        <div>
+            <button class="btn btn-primary rounded-pill" type="submit">VIEW ALL</button>
+            <button class="btn btn-primary rounded-pill" type="submit">LOOKBOOK</button>
+        </div>
+    </div>
 </div>
 </template>
 
@@ -21,5 +32,17 @@ export default {
     background-size: cover;
     background-position: center;
     position: relative;
+    color: white;
+    .ms_jumbotron-text{
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        text-align: center;
+        h1{
+            font-size: 7rem;
+        }
+    }
+
 }
 </style>
