e3b5c844788f4a5c22970a0030014d5d6accf1ed
mattesalva93 <salvalaggiomatteo93@gmail.com>
2022-02-09 18:22:45 +0100

    collection done

--- src/components/sections/Collection.vue
+++ src/components/sections/Collection.vue
@@ -1,24 +1,15 @@
 <template>
 
 <div class="ms_collection-container">
-    <div class="ms_collection-box">
+    <div v-for="(element, index) in collezioniArray" 
+        :key="index" 
+        class="ms_collection-box">
+        <img 
+        :src="element.src"
+        :alt="element.nome">
         <div class="ms_collection-text">
-            <h5>Winter Collection</h5>
-            <h6>Stylish and warm</h6>
-            <button class="btn btn-primary rounded-pill" type="submit">View More</button>
-        </div>
-    </div>
-    <div class="ms_collection-box">
-        <div class="ms_collection-text">
-            <h5>Winter Collection</h5>
-            <h6>Stylish and warm</h6>
-            <button class="btn btn-primary rounded-pill" type="submit">View More</button>
-        </div>
-    </div>
-    <div class="ms_collection-box">
-        <div class="ms_collection-text">
-            <h5>Winter Collection</h5>
-            <h6>Stylish and warm</h6>
+            <h5>{{element.nome}}</h5>
+            <h6>{{element.subnome}}</h6>
             <button class="btn btn-primary rounded-pill" type="submit">View More</button>
         </div>
     </div>
@@ -29,7 +20,29 @@
 
 <script>
 export default {
-    name:"Collection"
+    name:"Collection",
+    data(){
+        return{
+            collezioniArray: [
+                {
+                    nome: "Winter collection",
+                    subnome: "Stylish and warm",
+                    src: require("../../assets/img/winter_collection_bg.jpg")
+                },
+                {
+                    nome: "Spring collection",
+                    subnome: "Bright and colorful",
+                    src: require("../../assets/img/spring_collection_bg.jpg")
+                },
+                {
+                    nome: "Autumn collection",
+                    subnome: "Rich and comfortable",
+                    src: require("../../assets/img/autumn_collection_bg.jpg")
+                },
+            ]
+
+        }
+    }
 }
 </script>
 
@@ -43,12 +56,13 @@ export default {
 
         .ms_collection-box{
             width: 33.3333333%;
-            height: 800px;
-            background-image: url(../../assets/img/winter_collection_bg.jpg);
-            background-size: cover;
-            background-position: center;
+            height: 900px;
             position: relative;
             text-align: center;
+            img{
+                height: 100%;
+                width: 100%;
+            }
             .ms_collection-text{
                 position: absolute;
                 bottom: 20%;
