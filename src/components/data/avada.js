67d2373667cee0d68f228c165a1a42f041ce79fd
mattesalva93 <salvalaggiomatteo93@gmail.com>
2022-02-10 17:40:51 +0100

    better quality imges

--- src/components/data/avada.js
+++ src/components/data/avada.js
@@ -1,6 +1,6 @@
 const products = [
     {
-        src:  require("../../assets/img/black_elegant_leather_jacket-200x260.jpg"),
+        src:  require("../../assets/img/black_elegant_leather_jacket-400x520.jpg"),
         modello: "Black Leather Jacket",
         tipologia: "Men, Jackets, Jeans",
         prezzo: "235$",
@@ -15,7 +15,7 @@ const products = [
         stelline: 5,
     },
     {
-        src:  require("../../assets/img/black_leather_suit-200x260.jpg"),
+        src:  require("../../assets/img/black_leather_suit-400x520.jpg"),
         modello: "Black Leather Suit",
         tipologia: "Men, Jackets",
         prezzo: "176$",
@@ -26,7 +26,7 @@ const products = [
         bestseller: true
     },
     {
-        src:  require("../../assets/img/blue_jacket_and_white_stripe_tee-200x260.jpg"),
+        src:  require("../../assets/img/blue_jacket_and_white_stripe_tee-400x520.jpg"),
         modello: "Blue Jacket & Stripe Tee",
         tipologia: "Men, Jackets, Suits",
         prezzo: "580$",
@@ -37,7 +37,7 @@ const products = [
         
     },
     {
-        src:  require("../../assets/img/modern_black_leather_suit-200x260.jpg"),
+        src:  require("../../assets/img/modern_black_leather_suit-400x520.jpg"),
         modello: "Modern Black Leather Suit",
         tipologia: "Men, Jackets",
         prezzo: "96$",
@@ -47,7 +47,7 @@ const products = [
         featured: true
     },
     {
-        src:  require("../../assets/img/blue_leather_jacket-200x260.jpg"),
+        src:  require("../../assets/img/blue_leather_jacket-400x520.jpg"),
         modello: "Blue Jacket & Stripe Tee",
         tipologia: "Men, Jackets, Suits",
         prezzo: "80$",
@@ -58,7 +58,7 @@ const products = [
         onsale: true,
     },
     {
-        src:  require("../../assets/img/black_leather_jacket-200x260.jpg"),
+        src:  require("../../assets/img/black_leather_jacket-400x520.jpg"),
         modello: "Black, Leather, Jacket",
         tipologia: "Women, Jackets, Suits",
         prezzo: "106$",
@@ -71,7 +71,7 @@ const products = [
 
     },
     {
-        src:  require("../../assets/img/hipster_black_top-200x260.jpg"),
+        src:  require("../../assets/img/hipster_black_top-400x520.jpg"),
         modello: "Black Top",
         tipologia: "Women, Top, Shorts",
         prezzo: "56$",
@@ -83,7 +83,7 @@ const products = [
         bestseller: true
     },
     {
-        src:  require("../../assets/img/modern_love_tee-200x260.jpg"),
+        src:  require("../../assets/img/modern_love_tee-400x520.jpg"),
         modello: "White Shirt Black Skirt",
         tipologia: "Women, Shirt, Skirt",
         prezzo: "78$",
@@ -94,7 +94,7 @@ const products = [
         bestseller: true
     },
     {
-        src:  require("../../assets/img/spring_printed_dress-200x260.jpg"),
+        src:  require("../../assets/img/spring_printed_dress-400x520.jpg"),
         modello: "Green Flowers Dress",
         tipologia: "Women, Dress",
         prezzo: "185$",
@@ -107,7 +107,7 @@ const products = [
         stelline: 5
     },
     {
-        src:  require("../../assets/img/brown_dress_shoes-200x260.jpg"),
+        src:  require("../../assets/img/brown_dress_shoes-400x520.jpg"),
         modello: "Brown Suit Shoes",
         tipologia: "Men, Shoes",
         prezzo: "41$",
@@ -117,7 +117,7 @@ const products = [
         featured: true
     },
     {
-        src:  require("../../assets/img/casual_leather_belts-200x260.jpg"),
+        src:  require("../../assets/img/casual_leather_belts-400x520.jpg"),
         modello: "Leather Belt",
         tipologia: "Belt",
         prezzo: "25$",
@@ -127,7 +127,7 @@ const products = [
         featured: true
     },
     {
-        src:  require("../../assets/img/leather_gloves-200x260.jpg"),
+        src:  require("../../assets/img/leather_gloves-400x520.jpg"),
         modello: "Leather Gloves",
         tipologia: "Gloves",
         prezzo: "45$",
@@ -139,7 +139,7 @@ const products = [
         stelline: 5,
     },
     {
-        src:  require("../../assets/img/modern_leather_boots-200x260.jpg"),
+        src:  require("../../assets/img/modern_leather_boots-400x520.jpg"),
         modello: "Modern Leather Boots",
         tipologia: "Men, Shoes",
         prezzo: "50$",
