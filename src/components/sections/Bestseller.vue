807aa9f1ce57c7a5115fdf5b4210534956bc63d8
mattesalva93 <salvalaggiomatteo93@gmail.com>
2022-02-10 12:11:30 +0100

    structure done no css

--- src/components/sections/Bestseller.vue
+++ src/components/sections/Bestseller.vue
@@ -1,21 +1,20 @@
 <template>
 
-<div class="ms_container text-center">
-    <h1>Best Seller</h1>
-    <h6> Must have products from our top sellers</h6>
-    <div class="ms_model-carousel-container">
-        <div class="ms_carousel-buttons rx">
-            <i class="fas fa-chevron-right"></i>
+<div class="ms_carousel-container ms_container text-center">
+    <h1>Best Sellers</h1>
+    <h6> Brand new products from top designer</h6>
+    <div @click="scrollaDestra" class="ms_carousel-buttons rx">
+        <i class="fas fa-chevron-right"></i>
+    </div>
+    <div class="ms_model-carousel-container" id="carousel-container-1">
+        <div v-for="(element, index) in modelliBestSellers" 
+          :key="index" 
+          class="ms_carousel-img-container">
+          <img :src="element.src" alt="">
         </div>
-        <img 
-        v-for="(element, index) in modelliBestSellers" 
-        :key="index"
-        :src="element.src" alt=""
-        class="ms_carousel-off"
-        :class="{'ms_carousel-active': getIndiceAttivo(index)}" />
-        <div class="ms_carousel-buttons lx">
-            <i class="fas fa-angle-left"></i>
-        </div>
+    </div>
+    <div @click="scrollaSinsitra" class="ms_carousel-buttons lx">
+        <i class="fas fa-angle-left" ></i>
     </div>
 </div>
 
@@ -41,14 +40,12 @@ export default {
     getBestSeller (){
       this.modelliBestSellers = this.modelli.filter(element => element.bestseller);
     },
-    getIndiceAttivo (indice){
-      if(indice <= 4){
-        return true
-      }
-      else{
-        return false
-      }
+    scrollaDestra(){
+      document.getElementById('carousel-container-1').scrollLeft += 256;
     },
+    scrollaSinsitra(){
+      document.getElementById('carousel-container-1').scrollLeft -= 256;
+    }
   }
 }
 </script>
@@ -56,29 +53,36 @@ export default {
 <style lang="scss" scoped>
 @import '../../assets/style/globals.scss';
   .ms_model-carousel-container{
-        position: relative;
-        img{
-            width: 20%;
-        }
-        .ms_carousel-off{
-          display: none;
-        }
-        .ms_carousel-active{
-          display: inline-block;
-        }
-        .ms_carousel-buttons{
-            position: absolute;
-            top: 50%;
-            padding: 10px 6px;
-            background-color: lightgrey;
-            color: white;
-            font-size: 0.8rem;
-        }
-        .lx{
-            left: 1%;
-        }
-        .rx{
-            right: 1%;
-        }
+    display: flex;
+    flex: 0 0 auto;
+    width: 100%;
+    overflow-x: auto;
+    &::-webkit-scrollbar {
+    display: none;
+  }
+    .ms_carousel-img-container{
+      min-width: 20%;
+      img{
+      width: 100%;
+      }
+      }
     }
+
+  .ms_carousel-container{
+    position: relative;
+    .ms_carousel-buttons{
+      position: absolute;
+      top: 50%;
+      padding: 10px 6px;
+      background-color: lightgrey;
+      color: white;
+      font-size: 0.8rem;
+  }
+  .lx{
+      left: 1%;
+  }
+  .rx{
+      right: 1%;
+  }
+  }
 </style>
--- src/components/sections/Newarrivals.vue
+++ src/components/sections/Newarrivals.vue
@@ -1,20 +1,19 @@
 <template>
-<div class="text-center">
+<div class="ms_carousel-container text-center">
     <h1>New arrivals</h1>
     <h6> Brand new products from top designer</h6>
-    <div class="ms_model-carousel-container">
-        <div class="ms_carousel-buttons rx">
-            <i class="fas fa-chevron-right"></i>
+    <div @click="scrollaDestra" class="ms_carousel-buttons rx">
+        <i class="fas fa-chevron-right"></i>
+    </div>
+    <div class="ms_model-carousel-container" id="carousel-container-2">
+        <div v-for="(element, index) in modelliNewarrivals" 
+          :key="index" 
+          class="ms_carousel-img-container">
+          <img :src="element.src" alt="">
         </div>
-        <img 
-        v-for="(element, index) in modelliBestSellers" 
-        :key="index"
-        :src="element.src" alt=""
-        class="ms_carousel-off"
-        :class="{'ms_carousel-active': getIndiceAttivo(index)}" />
-        <div class="ms_carousel-buttons lx">
-            <i class="fas fa-angle-left"></i>
-        </div>
+    </div>
+    <div @click="scrollaSinsitra" class="ms_carousel-buttons lx">
+        <i class="fas fa-angle-left" ></i>
     </div>
 </div>
 
@@ -26,12 +25,11 @@
 import products from '../data/avada.js';
 
 export default {
-
-    name:"Newarrivals",
+  name:"Newarrivals",
     data(){
     return{
       modelli: products,
-      modelliBestSellers: [],
+      modelliNewarrivals: [],
     }
   },
   created (){
@@ -39,16 +37,14 @@ export default {
   },
   methods: {
     getBestSeller (){
-      this.modelliBestSellers = this.modelli.filter(element => element.bestseller);
+      this.modelliNewarrivals = this.modelli.filter(element => element.bestseller);
     },
-    getIndiceAttivo (indice){
-      if(indice <= 4){
-        return true
-      }
-      else{
-        return false
-      }
+    scrollaDestra(){
+      document.getElementById('carousel-container-2').scrollLeft += 509;
     },
+    scrollaSinsitra(){
+      document.getElementById('carousel-container-2').scrollLeft -= 509;
+    }
   }
 }
 </script>
@@ -56,31 +52,38 @@ export default {
 <style lang="scss" scoped>
 @import '../../assets/style/globals.scss';
 
-.ms_model-carousel-container{
-        position: relative;
-        img{
-            width: 20%;
-        }
-        .ms_carousel-off{
-          display: none;
-        }
-        .ms_carousel-active{
-          display: inline-block;
-        }
-        .ms_carousel-buttons{
-            position: absolute;
-            top: 50%;
-            padding: 10px 6px;
-            background-color: lightgrey;
-            color: white;
-            font-size: 0.8rem;
-        }
-        .lx{
-            left: 1%;
-        }
-        .rx{
-            right: 1%;
-        }
+  .ms_model-carousel-container{
+    display: flex;
+    flex: 0 0 auto;
+    width: 100%;
+    overflow-x: auto;
+    &::-webkit-scrollbar {
+    display: none;
+  }
+    .ms_carousel-img-container{
+      min-width: 20%;
+      img{
+      width: 100%;
+      }
+      }
     }
+
+  .ms_carousel-container{
+    position: relative;
+    .ms_carousel-buttons{
+      position: absolute;
+      top: 50%;
+      padding: 10px 6px;
+      background-color: lightgrey;
+      color: white;
+      font-size: 0.8rem;
+  }
+  .lx{
+      left: 1%;
+  }
+  .rx{
+      right: 1%;
+  }
+  }
 
 </style>
